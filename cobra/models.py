99101356fc9c1ef8dbc8aa333b43d01ba351f336
risa <lisaffang@gmail.com>
2016-11-09 18:57:39 -0600

    added choice_types

--- cobra/models.py
+++ cobra/models.py
@@ -15,6 +15,23 @@ class Choice(models.Model):
 	choice_text = models.CharField(max_length=200)
 	destination = models.ForeignKey('Question')
 	question = models.ForeignKey('Question', related_name='choices')
+	
+	# here are my choice types:
+	link = 'LNK'
+	sms = 'SMS'
+	email = 'EML'
+	braintree = 'BT'
+	choice_type_choices = (
+	    ('LNK', 'link'), # not sure why I need to make this a tuple
+	    ('SMS', 'sms'),
+	    ('EML', 'email'),
+	    ('BT', 'braintree'),
+    )
+	choice_type = models.CharField(
+		max_length=15,
+		choices=choice_type_choices,
+		default=link,
+	)
 
 	def __str__(self):
 		return self.choice_text # how to get it to return foreign key in shell too?
